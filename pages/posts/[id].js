6996d5e6ec5db4d82f4dc060d52a046093b95910
Ararchy01 <ararchy0621@gmail.com>
2021-01-25 13:40:36 -0800

    some progress

---
+++ pages/posts/[id].js
@@ -0,0 +1,32 @@
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export async function getStaticPaths({ params }) {
+  const paths = await getAllPostIds(params.id);
+  return {
+    paths,
+    fallback: false,
+  };
+}
+const Post = ({ postData }) => {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  );
+};
+
+export default Post;
