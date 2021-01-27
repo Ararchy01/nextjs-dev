f226723c5c5e9aa8bfb767a6b17079294ebd66e4
Ararchy01 <ararchy0621@gmail.com>
2021-01-26 20:30:53 -0800

    maintain

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,15 +31,17 @@ export default function Home({
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
+            <Link href={`/posts/${id}`}>
+              <a>
+                <li className={utilStyles.listItem} key={id}>
+                  {title}
+                  <br />
+                  <small className={utilStyles.lightText}>
+                    <Date dateString={date} />
+                  </small>
+                </li>
+              </a>
+            </Link>
           ))}
         </ul>
       </section>
