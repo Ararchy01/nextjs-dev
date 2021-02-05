e7d05c9b4428c0658fbd7d689af09a8cc6c5cf64
Ararchy01 <ararchy0621@gmail.com>
2021-02-04 18:40:53 -0800

    Revised button

--- components/atoms/Button.tsx
+++ components/atoms/Button.tsx
@@ -1,6 +1,6 @@
 export default function Button() {
   return (
-    <button className="py-3 px-5 font-semibold rounded-lg shadow-md text-white bg-yellow-400 hover:bg-pink-300">
+    <button className="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-yellow-400 hover:bg-pink-300">
       Click me
     </button>
   );
