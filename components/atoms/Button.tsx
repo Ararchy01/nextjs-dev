8c8d566598d01a3048a9eaf18299fa4f315b97f5
Ararchy01 <ararchy0621@gmail.com>
2021-02-05 22:59:34 -0800

    little fix

--- components/atoms/Button.tsx
+++ components/atoms/Button.tsx
@@ -1,6 +1,6 @@
 export default function Button({ value }: { value: string }) {
   return (
-    <button className="btn text-gray-900 bg-yellow-300  hover:bg-yellow-200 outline-none">
+    <button className="btn text-gray-900 bg-yellow-300  hover:bg-yellow-200">
       {value}
     </button>
   );
