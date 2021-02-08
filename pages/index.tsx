84fcf133e71e937fa753de8ade17b88c7b5efaad
Ararchy01 <ararchy0621@gmail.com>
2021-02-07 23:41:05 -0800

    Reconstruct components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 import Head from "next/head";
-import Layout, { siteTitle } from "../components/layout";
+import Layout, { siteTitle } from "../components/templates/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
-import Date from "../components/date";
+import Date from "../components/atoms/date";
 import { GetStaticProps } from "next";
-import Button from "../components/atoms/Button";
 import OkCancel from "../components/molecules/OkCancel";
 
 export default function Home({
