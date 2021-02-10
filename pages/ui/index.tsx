ce8474c652d25a6992b072787966356ff37790f8
Ararchy01 <ararchy0621@gmail.com>
2021-02-09 23:25:13 -0800

    disabled styles

--- pages/ui/index.tsx
+++ pages/ui/index.tsx
@@ -1,17 +1,28 @@
+import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Button from "../../components/atoms/Button";
 import DatePicker from "../../components/atoms/DatePicker";
 import TextInput from "../../components/atoms/TextInput";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+}));
+
 export default function UI() {
+  const classes = useStyles();
   return (
-    <Paper elevation={1}>
-      <Paper elevation={3}>
-        <h1>Atoms</h1>
-        <Button />
-        <DatePicker />
-        <TextInput />
+    <div>
+      <Paper elevation={5}>
+        <Paper elevation={3}>
+          <h1>Atoms</h1>
+          <Button />
+          <DatePicker />
+          <TextInput />
+        </Paper>
       </Paper>
-    </Paper>
+    </div>
   );
 }
